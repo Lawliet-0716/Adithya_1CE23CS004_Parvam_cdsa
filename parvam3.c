20a8c41c291f8ef9d6c321f1fff847fe73398cb0
Lawliet-0716 <sadithya077@gmail.com>
2024-11-07 22:39:37 +0530

    Delete parvam3.c

--- parvam3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    float flt;
-    int itg;
-    printf("Enter the float:");
-    scanf("%f",&flt);
-    itg = (int)flt;
-    printf("Converted float to integer :%d \n ",itg);
-}
